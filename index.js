6001a8954f0a933e36bd0f0c5e582605e0642a74
MSoulSol <masrimizo@gmail.com>
2022-03-16 05:10:41 -0400

    added the question prompts for monthly expenses via more for another for loop

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ let numberAnswer = parseFloat(StringAnswer)
 console.log(numberAnswer)
 weeklyExpenses = weeklyExpenses+numberAnswer 
 }
+
+for(i=0; i<monthlyExpenseQuestions.length; i++){let StringAnswer = window.prompt(monthlyExpenseQuestions[i])
+let numberAnswer = parseFloat(StringAnswer)
+console.log(numberAnswer)
+monthlyExpenses = monthlyExpenses + numberAnswer
+}
